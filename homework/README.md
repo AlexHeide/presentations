7f8c2be13a1552c59a93348353a481bbaf7b5460
Alexander Heide <alexander.heide@uga.edu>
2025-06-11 14:27:25 -0400

    Add easy grep homework

---
+++ homework/README.md
@@ -0,0 +1,8 @@
+This directory simulates running optavc, but I didn't run it on a cluster so it failed.
+Write a bash function that when exectued in the GRAD directory prints the displacement number
+(or the number of the directory) that has failed. HINT: search the file in some way (grep)
+
+When psi4 exits it either prints:
+*** Psi4 encountered an error. Buy a developer more coffee!
+or
+*** Psi4 exiting successfully. Buy a developer a beer!
---
+++ homework/main.py
@@ -0,0 +1,25 @@
+
+options_kwargs = {
+  'template_file_path': "template.dat",
+  'energy_regex'      : r"@DF-RHF Final Energy:\s+(-\d+\.\d+)",
+  'program'           : "psi4",
+  'wait_time'         : 120,
+  'input_name'        : "input.dat",
+  'output_name'       : "output.dat",
+  'queue': 'batch',
+  'cluster': 'Sapelo',
+}
+
+from optavc import options
+options_obj = options.Options(**options_kwargs)
+
+from optavc.template import TemplateFileProcessor
+tfp = TemplateFileProcessor(open("template.dat").read(), options_obj)
+
+from optavc.findifcalcs import Gradient
+import numpy as np
+grad_obj = Gradient(tfp.molecule, tfp.input_file_object, options_obj, path="GRAD")
+grad_obj.compute_result()
+grad = grad_obj.reap()
+enrg = grad_obj.get_reference_energy()
+
